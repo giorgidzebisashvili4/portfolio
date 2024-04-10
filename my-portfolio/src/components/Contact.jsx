1fc155a9aa1d4e2e9add457e680c5e5d35e0bab6
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2024-04-10 19:03:45 +0400

    add contact text ideas

--- my-portfolio/src/components/Contact.jsx
+++ my-portfolio/src/components/Contact.jsx
@@ -5,8 +5,11 @@ import styles from "./Contact.module.css";
 function Contact() {
   return (
     <section id="contact" className={styles.contact}>
-      <h2>Contact Me</h2>
-      <form>{/* Your contact form here */}</form>
+      <h2>Want to get in touch?</h2>
+      <h3>phone : +995 598 60 41 39</h3>
+      <h3>mail giorgidzebisashvili4@gmail.com</h3>
+      <h3>linkedin</h3>
+      <h3>github</h3>
     </section>
   );
 }
