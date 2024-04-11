d0aca45ecd84f5445931c5c7c6b334bfd0f755dd
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2024-04-11 19:51:10 +0400

    add project logic

--- my-portfolio/src/components/Projects.jsx
+++ my-portfolio/src/components/Projects.jsx
@@ -1,12 +1,25 @@
 // src/components/Projects.jsx
 import React from "react";
 import styles from "./Projects.module.css";
+import weatherApp from "../assets/project-images/weatherApp.png";
 
 function Projects() {
   return (
     <section id="projects" className={styles.projects}>
-      <h2>Projects</h2>
-      <div>{/* Your projects here */}</div>
+      <img src={weatherApp} alt="My Photo" />
+      <div className="about-text-section">
+        <h2>wather app</h2>
+        <p>small description about this project</p>
+        <p>tooles used: sass, react, webpack</p>
+        <div className="button-links">
+          <a href="" target="_blank" rel="">
+            <img src="" alt="git" />
+          </a>
+          <a href="" target="_blank" rel="">
+            <img src="" alt="live" />
+          </a>
+        </div>
+      </div>
     </section>
   );
 }
