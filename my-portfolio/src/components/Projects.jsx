7710c1a47ec06ce58849797be93ee8d764f7efc7
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2024-04-12 14:45:13 +0400

    finished project section

--- my-portfolio/src/components/Projects.jsx
+++ my-portfolio/src/components/Projects.jsx
@@ -2,21 +2,30 @@
 import React from "react";
 import styles from "./Projects.module.css";
 import weatherApp from "../assets/project-images/weatherApp.png";
+import git from "../assets/icons/btns/git.svg";
+import openlink from "../assets/icons/btns/openlink.svg";
 
 function Projects() {
   return (
     <section id="projects" className={styles.projects}>
       <img src={weatherApp} alt="My Photo" />
-      <div className="about-text-section">
-        <h2>wather app</h2>
-        <p>small description about this project</p>
-        <p>tooles used: sass, react, webpack</p>
+      <div className={styles["about-text-section"]}>
+        <div className="project-description">
+          <h2>wather app</h2>
+          <p>
+            small description about this project sadasd dads Lorem ipsum dolor
+            sit amet consectetur adipisicing elit. Excepturi nostrum nesciunt
+            sunt aut dolores amet alias earum sint placeat nemo facere molestias
+            nam similique atque laudantium, optio et ullam numquam?
+          </p>
+          <p>tooles used: sass, react, webpack</p>
+        </div>
         <div className="button-links">
           <a href="" target="_blank" rel="">
-            <img src="" alt="git" />
+            <img src={git} alt="git" />
           </a>
           <a href="" target="_blank" rel="">
-            <img src="" alt="live" />
+            <img src={openlink} alt="live" />
           </a>
         </div>
       </div>
