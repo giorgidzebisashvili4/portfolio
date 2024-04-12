fb604256d78a981daac43522d0af73f33391af9e
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2024-04-12 15:31:07 +0400

    finished contact layout and added borders in skill titles

--- my-portfolio/src/components/Contact.jsx
+++ my-portfolio/src/components/Contact.jsx
@@ -1,15 +1,66 @@
 // src/components/Contact.jsx
 import React from "react";
 import styles from "./Contact.module.css";
+import call from "../assets/icons/contact/call.svg";
+import mail from "../assets/icons/contact/mail.svg";
+import git from "../assets/icons/contact/git.svg";
+import linkedin from "../assets/icons/contact/linkedin.svg";
 
 function Contact() {
   return (
     <section id="contact" className={styles.contact}>
       <h2>Want to get in touch?</h2>
-      <h3>phone : +995 598 60 41 39</h3>
-      <h3>mail giorgidzebisashvili4@gmail.com</h3>
-      <h3>linkedin</h3>
-      <h3>github</h3>
+      <div className={styles["contact-links"]}>
+        <div>
+          <div>
+            <img src={call} alt="mail" />
+            <a href="" target="_blank" rel="">
+              +995 598 60 41 39
+            </a>
+          </div>
+          <div>
+            <img src={mail} alt="mail" />
+            <a href="" target="_blank" rel="">
+              giorgidzebisashvili4@gmail.com
+            </a>
+          </div>
+        </div>
+        <div>
+          <div>
+            <a
+              href="https://www.linkedin.com/in/giorgi-dzebisashvili-612bbb180/"
+              target="_blank"
+              rel=""
+            >
+              <img src={linkedin} alt="mail" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/giorgi-dzebisashvili-612bbb180/"
+              target="_blank"
+              rel=""
+            >
+              linkedin
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://github.com/giorgidzebisashvili4"
+              target="_blank"
+              rel=""
+            >
+              <img src={git} alt="mail" />
+            </a>
+
+            <a
+              href="https://github.com/giorgidzebisashvili4"
+              target="_blank"
+              rel=""
+            >
+              github
+            </a>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
