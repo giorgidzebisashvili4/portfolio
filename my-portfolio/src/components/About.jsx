685f4159fe95c036d1528aba0a2d5c81f7b75103
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2024-04-24 17:13:04 +0400

    paddings on header and about section fixed

--- my-portfolio/src/components/About.jsx
+++ my-portfolio/src/components/About.jsx
@@ -2,36 +2,38 @@
 // src/components/About.jsx
 import React from "react";
 import styles from "./About.module.css";
-import myPhoto from "../assets/images/my-photo.jpeg";
+import myPhoto from "../assets/images/my-photo.jpg";
 
 function About() {
   return (
     <section id="about" className={styles.about}>
-      <img src={myPhoto} alt="My Photo" />
-      <div className="about-text-section">
-        <h2>Giorgi Dzebisashvili</h2>
-        <h4>React Developer & Graphic Designer</h4>
-        <p>
-          Hello! I’m Giorgi Dzebisashvili, a versatile creative professional
-          passionate about both graphic design and full-stack development. With
-          a solid foundation in graphic design, I've crafted engaging visuals
-          across diverse industries, refining my eye for detail and user-centric
-          design.
-          <br />
-          <br />
-          Driven by a hunger for new challenges, I recently delved into
-          full-stack development. Through dedicated learning, I've mastered
-          technologies like React.js and begun integrating design principles
-          into my coding process. Each project reflects my commitment to
-          innovation and my determination to push boundaries.
-          <br />
-          <br />
-          Explore my portfolio to see how I blend creativity with technical
-          expertise, creating seamless digital experiences. I’m always open to
-          collaboration and eager to discuss how we can turn your ideas into
-          reality. Let’s connect and bring your vision to life!
-        </p>
-        <a href="#contact">Contact Me</a>
+      <div className="about-container">
+        <img src={myPhoto} alt="My Photo" />
+        <div className="about-text-section">
+          <h2>Giorgi Dzebisashvili</h2>
+          <h4>React Developer & Graphic Designer</h4>
+          <p>
+            Hello! I’m Giorgi Dzebisashvili, a versatile creative professional
+            passionate about both graphic design and full-stack development.
+            With a solid foundation in graphic design, I've crafted engaging
+            visuals across diverse industries, refining my eye for detail and
+            user-centric design.
+            <br />
+            <br />
+            Driven by a hunger for new challenges, I recently delved into
+            full-stack development. Through dedicated learning, I've mastered
+            technologies like React.js and begun integrating design principles
+            into my coding process. Each project reflects my commitment to
+            innovation and my determination to push boundaries.
+            <br />
+            <br />
+            Explore my portfolio to see how I blend creativity with technical
+            expertise, creating seamless digital experiences. I’m always open to
+            collaboration and eager to discuss how we can turn your ideas into
+            reality. Let’s connect and bring your vision to life!
+          </p>
+          <a href="#contact">Contact Me</a>
+        </div>
       </div>
     </section>
   );
--- my-portfolio/src/components/Header.jsx
+++ my-portfolio/src/components/Header.jsx
@@ -5,23 +5,25 @@ import styles from "./Header.module.css";
 function Header() {
   return (
     <header className={styles.header}>
-      <h1>Giorgi Dzebisashvili</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#skills">Skills</a>
-          </li>
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
+      <div className="container">
+        <h1>Giorgi Dzebisashvili</h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#skills">Skills</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
