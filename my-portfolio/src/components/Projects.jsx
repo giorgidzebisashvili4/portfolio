0099b13be1253be36a9417c37ed6eebdb82fdcb4
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2024-04-25 18:41:09 +0400

    add style in projects and added this website as project

--- my-portfolio/src/components/Projects.jsx
+++ my-portfolio/src/components/Projects.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import styles from "./Projects.module.css";
 import weatherApp from "../assets/project-images/weatherApp.png";
+import portfolioPhoto from "../assets/project-images/portfolio.png";
 import git from "../assets/icons/btns/git.svg";
 import openlink from "../assets/icons/btns/openlink.svg";
 
@@ -9,7 +10,9 @@ function Projects() {
   return (
     <section id="projects" className={styles.projects}>
       <div>
-        <img src={weatherApp} alt="My Photo" />
+        <a href="http://localhost:5173/" target="_blank" rel="">
+          <img src={weatherApp} alt="My Photo" />
+        </a>
         <div className={styles["about-text-section"]}>
           <div className="project-description">
             <h2>wather app</h2>
@@ -19,13 +22,49 @@ function Projects() {
               sunt aut dolores amet alias earum sint placeat nemo facere
               molestias nam similique atque laudantium, optio et ullam numquam?
             </p>
-            <p>tooles used: sass, react, webpack</p>
+            <p>
+              {" "}
+              build with: <span>vite</span>
+              <span>react</span>
+            </p>
           </div>
           <div className="button-links">
             <a href="" target="_blank" rel="">
               <img src={git} alt="git" />
             </a>
             <a href="" target="_blank" rel="">
+              <img src={openlink} alt="live" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div>
+        <a href="http://localhost:5173/" target="_blank" rel="">
+          <img src={portfolioPhoto} alt="My Photo" />
+        </a>
+        <div className={styles["about-text-section"]}>
+          <div className="project-description">
+            <h2>this portfolio website</h2>
+            <p>
+              small description about this project sadasd dads Lorem ipsum dolor
+              sit amet consectetur adipisicing elit. Excepturi nostrum nesciunt
+              sunt aut dolores amet alias earum sint placeat nemo facere
+              molestias nam similique atque laudantium, optio et ullam numquam?
+            </p>
+            <p>
+              build with: <span>vite</span>
+              <span>react</span>
+            </p>
+          </div>
+          <div className="button-links">
+            <a
+              href="https://github.com/giorgidzebisashvili4/portfolio"
+              target="_blank"
+              rel=""
+            >
+              <img src={git} alt="git" />
+            </a>
+            <a href="http://localhost:5173/" target="_blank" rel="">
               <img src={openlink} alt="live" />
             </a>
           </div>
