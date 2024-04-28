2baa3aafa72d284f3df5f718e737e6d540754c33
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2024-04-28 17:07:18 +0400

    fixed skills mobile version

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -36,95 +36,99 @@ function Skills() {
           </p>
         </div>
         <div className={styles["skill-icon"]}>
-          <div className={styles["front-end"]}>
-            <h3>Front End</h3>
-            <div className={styles["front-end-icons"]}>
-              <div>
-                <h4>html</h4>
-                <img src={html} alt="html Logo" />
+          <div className={styles["right-side"]}>
+            <div className={styles["front-end"]}>
+              <h3>Front End</h3>
+              <div className={styles["front-end-icons"]}>
+                <div>
+                  <h4>html</h4>
+                  <img src={html} alt="html Logo" />
+                </div>
+                <div>
+                  <h4>css</h4>
+                  <img src={css} alt="css Logo" />
+                </div>
+                <div>
+                  <h4>JavaScript</h4>
+                  <img src={jsSvg} alt="JavaScript Logo" />
+                </div>
+                <div>
+                  <h4>React</h4>
+                  <img src={react} alt="react Logo" />
+                </div>
+                <div>
+                  <h4>Vite</h4>
+                  <img src={vite} alt="vite Logo" />
+                </div>
+                <div>
+                  <h4>Sass</h4>
+                  <img src={sass} alt="sass Logo" />
+                </div>
+                <div>
+                  <h4>Tailwind</h4>
+                  <img src={tailwind} alt="tailwind Logo" />
+                </div>
               </div>
-              <div>
-                <h4>css</h4>
-                <img src={css} alt="css Logo" />
-              </div>
-              <div>
-                <h4>JavaScript</h4>
-                <img src={jsSvg} alt="JavaScript Logo" />
-              </div>
-              <div>
-                <h4>React</h4>
-                <img src={react} alt="react Logo" />
-              </div>
-              <div>
-                <h4>Vite</h4>
-                <img src={vite} alt="vite Logo" />
-              </div>
-              <div>
-                <h4>Sass</h4>
-                <img src={sass} alt="sass Logo" />
-              </div>
-              <div>
-                <h4>Tailwind</h4>
-                <img src={tailwind} alt="tailwind Logo" />
+            </div>
+            <div className="back-tools">
+              <h3>Back End</h3>
+              <div className={styles["back-end-icons"]}>
+                <div>
+                  <h4>NodeJs</h4>
+                  <img src={nodejs} alt="nodejs Logo" />
+                </div>
+                <div>
+                  <h4>Express</h4>
+                  <img src={express} alt="express Logo" />
+                </div>
+                <div>
+                  <h4>MongoDB</h4>
+                  <img src={mongoDB} alt="mongoDB Logo" />
+                </div>
               </div>
             </div>
           </div>
-          <div className="back-tools">
-            <h3>Back End</h3>
-            <div className={styles["back-end-icons"]}>
-              <div>
-                <h4>NodeJs</h4>
-                <img src={nodejs} alt="nodejs Logo" />
-              </div>
-              <div>
-                <h4>Express</h4>
-                <img src={express} alt="express Logo" />
-              </div>
-              <div>
-                <h4>MongoDB</h4>
-                <img src={mongoDB} alt="mongoDB Logo" />
-              </div>
-            </div>
-          </div>
-          <div className="development-tools">
-            <h3>development Tools</h3>
-            <div className={styles["development-tools-icons"]}>
-              <div>
-                <h4>Git</h4>
-                <img src={git} alt="git Logo" />
-              </div>
-              <div>
-                <h4>npm</h4>
-                <img src={npm} alt="npm Logo" />
-              </div>
-              <div>
-                <h4>Webpack</h4>
-                <img src={webpack} alt="webpack Logo" />
-              </div>
-              <div>
-                <h4>Jest</h4>
-                <img src={jest} alt="jest Logo" />
+          <div className={styles["left-side"]}>
+            <div className="development-tools">
+              <h3>development Tools</h3>
+              <div className={styles["development-tools-icons"]}>
+                <div>
+                  <h4>Git</h4>
+                  <img src={git} alt="git Logo" />
+                </div>
+                <div>
+                  <h4>npm</h4>
+                  <img src={npm} alt="npm Logo" />
+                </div>
+                <div>
+                  <h4>Webpack</h4>
+                  <img src={webpack} alt="webpack Logo" />
+                </div>
+                <div>
+                  <h4>Jest</h4>
+                  <img src={jest} alt="jest Logo" />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="design-tools">
-            <h3>Design Tools</h3>
-            <div className={styles["design-tools-icons"]}>
-              <div>
-                <h4>Figma</h4>
-                <img src={figma} alt="figma Logo" />
-              </div>
-              <div>
-                <h4>Photoshop</h4>
-                <img src={photoshop} alt="photoshop Logo" />
-              </div>
-              <div>
-                <h4>Illustrator</h4>
-                <img src={illustrator} alt="illustrator Logo" />
-              </div>
-              <div>
-                <h4>After Effects</h4>
-                <img src={aftereffects} alt="aftereffects Logo" />
+            <div className="design-tools">
+              <h3>Design Tools</h3>
+              <div className={styles["design-tools-icons"]}>
+                <div>
+                  <h4>Figma</h4>
+                  <img src={figma} alt="figma Logo" />
+                </div>
+                <div>
+                  <h4>Photoshop</h4>
+                  <img src={photoshop} alt="photoshop Logo" />
+                </div>
+                <div>
+                  <h4>Illustrator</h4>
+                  <img src={illustrator} alt="illustrator Logo" />
+                </div>
+                <div>
+                  <h4>After Effects</h4>
+                  <img src={aftereffects} alt="aftereffects Logo" />
+                </div>
               </div>
             </div>
           </div>
